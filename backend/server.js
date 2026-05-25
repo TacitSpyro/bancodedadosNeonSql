57d1dd1811ff61cce61b0ea48d721e6b7055477d
Serj Tankian <phmundel2009@gmail.com>
2026-05-25 10:27:17 -0300

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,96 +1,96 @@
+require("dotenv").config
 const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
 
 const app = express();
+const port = process.env.port || 5000;
 
+app.use(cors());
 app.use(express.json());
 
-let usuarios = [
-    {id: 1, nome: "João", email: "joao@gmail.com"},
-    {id: 2, nome: "Maria", email: "maria@gmail.com"},
-    {id: 3, nome: "Carlos", email: "carlos@gmail.com"}
-];
-
-app.get("/", (req,res) => {
-    res.send("Servidor funcionando com sucesso");
+const pool = new Pool ({
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.DATABASE_URL ? { rejectUnauthorized:false }: false,
 });
 
-app.get("/usuarios", (req,res) => {
-    res.json(usuarios);
-});
-
-app.get("/usuarios/:id", (req, res) => {
-    const id = Number(req.params.id);
-
-    const usuario = usuarios.find(u => u.id === id);
-
-    if (!usuario){
-        return res.status(404).json({
-            mensagem: "usuario não encontrado"
-        })
+async function initDB() {
+    try{
+        await pool.querry(
+            `CREATE TABLE IF NOT EXISTS produtos{
+                id SERIAL PRIMARY KEY,
+                nome VARCHAR(255) NOT NULL,
+                quantidade INTEGER NOT NULL,
+                preco NUMERIC(10,2) NOT NULL
+            }`
+        )
+        console.log("Tabela verificada/criada com sucesso")
+    }catch(err){
+        console.error("Erro ao criar tabela",err);
     }
-    res.json(usuario);
+}
+
+initDB(); 
+
+app.get("/", (req, res) =>{
+    res.send(`
+        <h2> API de Controle de Estoque<h2>
+        <p>API Funcionado Corretamente!<p>
+        <a href="/api/produtos">ver produtos</a>
+        `);
+})
+
+const BASE_URL = "/api/produtos";
+
+app.get(BASE_URL, async(req,res) => {
+    try{
+        const result = await pool.querry(
+            "SELECT * FROM produtos ORDER BY id DESC"
+        );
+        res.json(result.rows);
+    }catch(err){
+        res.status(500).json({ error: "Erro ao buscar produtos"});
+    }
 });
 
+app.post(BASE_URL, async(req, res) => {
+    try{
+        const {nome, quantidade, preco} = req.body;
 
-app.post("/cadastro", (req,res) => {
-    const { nome, email } = req.body;
+        const result = await pool.querry(
+            "ISERT INTO produtos (nome, quantidade, preco) getRandomValues($1, $2, $3) RETURNING *", [nome, quantidade, produto]
+        );
+        res.status(201).json(result.rows[0])
+    }catch(err){
+        res.status(500).json({error: "Erro ao inserir produtos"});
+    }
+});
 
-    if(!nome || !email){
-        return res.status(400).json({
-            mensagem: "Nome e email são obrigatorios"
+app.put(`${BASE_URL}/:id`, async(req, res) => {
+    try{
+        const { id } = req.params;
+        const { nome, quantidade, preco } = req.body(
+            `UPDATE produtos SET nome=$1, quantidade=$2, WHERE id=$4 RETURNING *`,
+            [nome, quantidade, preco, id]
+        );
+        
+        res.json(result.rows[0]);
+    }catch(err){
+        res.status(500).json({
+            error: "Erro ao atualizar o produto"
         });
     }
-    const novoUsuario = {
-        id:usuarios.length + 1,
-        nome,
-        email
-    };
-    usuarios.push(novoUsuario);
-
-    res.status(201).json({
-        mensagem: "Cadastro realizado com sucesso!",
-        usuario: novoUsuario
-    });
 })
 
-app.put("/editar/:id", (req,res) => {
-    const id = Number(req.params.id);
-    const { nome, email } = req.body;
-    const usuario = usuarios.find(u => u.id === id);
-    if(!usuario){
-        return res.status(404).json({
-            mensagem: "Usuario não encontrado"
-        })
+app.delete(`${BASE_URL}/:id`, async(req, res) =>{
+    try{
+        const {id} = req.params;
+
+        await pool.querry("DELETE FROM produto WHERE id=$1", {id});
+        res.json({message: "Produto removido com sucesso!"})
+    }catch(err){
+        res.status(500).json({error: "Erro ao remover produto"});
     }
-
-    if(nome) usuario.nome = nome;
-    if(email)usuario.email = email;
-
-    res.json({
-        mensagem: "Usuario atualizado com sucesso",
-        usuario
-    })
-});
-
-app.delete("/deletar/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const index = usuarios.findIndex( u => u.id === id);
-    if(index === -1){
-        return res.status(404).json({
-            mensagem: "Usuario não encontrado"
-        })
-    }
-
-    const usuarioRemovido = usuarios.splice(index, 1);
-
-    res.json({
-        mensagem: "Usuario deletado com sucesso!",
-        usuario: usuarioRemovido[0]
-    })
-});
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
 })
+
+app.listen(port, () => console.log(`Sevidor Rodando em http://localhost:${port}`))
